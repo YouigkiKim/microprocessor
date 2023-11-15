f638b46dbec5a57241c60e13dde37bb1792ec160
YoungkiKim <gkdlgkdl1231@gmail.com>
2023-11-15 21:01:38 +0900

    add BTcode

---
+++ BT.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTserial(2,3); // RX, TX
+uint8_t data = 0x00;
+
+void setup() {
+  Serial.begin(9600);
+  BTserial.begin(9600);
+}
+
+void loop() {
+
+  if(BTserial.available()){
+    Serial.write(BTserial.read());
+    
+  }
+
+  if( Serial.available() ){
+    BTserial.write( Serial.read() );
+   
+  }
+}
