ff884d8047dbd604c395c7e7316c2822cec90b51
YoungkiKim <gkdlgkdl1231@gmail.com>
2023-12-07 00:43:17 +0900

    main code done

--- Main/bt_fin_plusflag/bt_fin_plusflag.ino
+++ Main/bt_fin_plusflag/bt_fin_plusflag.ino
@@ -7,7 +7,6 @@
 #include "Stepper.h"
 
 Servo windowServo;
-
 Servo airConditionerServo;
 
 #define STEPPERREV 2048
@@ -30,12 +29,25 @@ int VentmotorPin = 10;             // 환풍기 DC 모터가 연결된 핀
 
 int airConledPin = 12;             // 에어컨 내부 led 핀
 
+//창문상태지정 True=닫힘 False=열림
+bool WindowClose = true;;
+bool WindowAuto = true;
+bool WindowState = WindowClose;           //창문상태지정 True=닫힘 False=열림
+bool WindowState = WindowClose; 
+//창문상태지정 True=닫힘 False=열림
+bool WindowClose = true;;
+bool WindowAuto = true;
+bool WindowState = WindowClose;           //창문상태지정 True=닫힘 False=열림
+bool WindowState = WindowClose; 
+unsigned long startTime;
+unsigned long currentTime;
 int brightness; //보일러LED 밝기 변수 선언
 int irSensorValue; // irSensorValue 변수 선언 
+char data;
+int ventflag = 0;
 
 const uint8_t pin_in1 = 4;  
 const uint8_t pin_in2 = 11;
-
 uint8_t rpm = 8;
 
 Stepper myStepper(STEPPERREV, pin_in1, pin_in2);
@@ -68,111 +80,98 @@ void loop() {
 
   // Bluetooth Serial로부터 데이터를 읽어들임
   if (BTSerial.available()) {
+
     // 읽은 데이터를 변수에 저장
-    char data = BTSerial.read();
-    // 읽은 데이터를 시리얼 모니터로 출력
+    data = BTSerial.read();
+
+    // 읽은 데이터를 시리얼 모니터로 출력, 테스트용 코드
     Serial.write(data);
+
     // 스마트폰 신호가 'a'일 경우 (방범장치 제어)
     if (data == 'a') {
-      // Bluetooth Serial로부터 추가 데이터가 있는 동안 반복
-      while (BTSerial.available()) {
-        // 아날로그 핀을 통해 IR 센서의 값을 읽어들임
-        int irSensorValue = analogRead(irSensorPin);
-        // 이후 두번째 스마트폰 신호가 '1'인 경우 (방범장치 AUTO모드 작동)
-        if (data == '1') {
-          if (irSensorValue < 200) {
-            activateAlarm();
-            delay(500);
-          } 
-          else {            
-            noTone(buzzerPin);
-            delay(500);
-            digitalWrite(ledPin, LOW);
-            delay(500);
-          }
-        }
-        // 이후 두번째 스마트폰 신호가 '0'인 경우 (방범장치 수동 off) 
-        else if (data == '0') {
-          // 방범 소리 및 빛 전부 끄기
-          
-          noTone(buzzerPin);
-          delay(500);
-          digitalWrite(ledPin, LOW);
-          delay(500);
-          
-        }
-      }  
+
+      while(!BTSerial.available()); // polling방식으로 두번째 데이터를 기다림
+      data = BTSerial.read();
+
+      if(BTdata == 'on'){
+        Security = SecurityOn;
+      }
+      else if(BTdata == 'off'){
+        Security = SecurityOff;
+      }
     }
   
     // 스마트폰 신호가 'b'일 경우 (환풍기 제어)
     else if (data == 'b') {
-      // Bluetooth Serial로부터 추가 데이터가 있는 동안 반복
-      while (BTSerial.available()) {
-        // 이후 두번째 스마트폰 신호가 'a'인 경우 (환풍기 제어 AUTO모드 작동)
-        if (data == 'a') {
-          int ventflag = 1;
-          if (humidity > 70) { 
-            startVentMotor();                 
-          } 
-          else {
-            stopVentMotor();
-          }
-        }
-        // 이후 두번째 스마트폰 신호가 'b나 c'인 경우 (환풍기 제어 스마트폰 수동 ON/OFF)
-        else if (data == 'b') {
-          analogWrite(VentmotorPin, 255);
-          int ventflag = 0;
-        }
-        else if (data == 'c') {
-          analogWrite(VentmotorPin, 0);
-          int ventflag = 0;
-        }
+      while (!BTSerial.available());
+      // 이후 두번째 스마트폰 신호가 'a'인 경우 (환풍기 제어 AUTO모드 작동)
+      if (data == 'a') {
+        ventflag = 1;
+      else{
+        ventflag = 0;
       }
+      }
+
+      // 이후 두번째 스마트폰 신호가 'b나 c'인 경우 (환풍기 제어 스마트폰 수동 ON/OFF)
+      else if (data == 'b') {
+        analogWrite(VentmotorPin, 255);
+        int ventflag = 0;
+      }
+      else if (data == 'c') {
+        analogWrite(VentmotorPin, 0);
+        int ventflag = 0;
+      }
+      
     }
-  // 스마트폰 신호가 'c'일 경우 (window 제어)
+
+    // 스마트폰 신호가 'c'일 경우 (window 제어)
     else if (data == 'c') {
-      while (BTSerial.available()) {
-        if (data == '1') {
-          openWindow();
-        }
-        else if (data == '0') {
-          closeWindow();
-        }
+      while (!BTSerial.available()); //polling방식으로 두번째 데이터 수신을 기다림.
+      data = BTSerial.read();
+      if(data = ){
+        openWindow();
+      }
+      else{
+        closeWindow();
       }
     }
     
     // 스마트폰 신호가 'f' 일 경우 (보일러와 에어컨 제어)
     else if (data == 'f') {
-      while (BTSerial.available()) {
-    
-        if (data == 'a') {
-          int tempflag = 1;
-          if (desiredTemperature > currentTemperature) {
-            setBoilerTemperature(desiredTemperature, currentTemperature);
-          }
-          else{
-            autoActivateAirConditioner();
-          }
+      while (!BTSerial.available());
+      data = BTSerial.read();
+
+      if (data == 'a') {
+        int tempflag = 1;
+        if (desiredTemperature > currentTemperature) {
+          setBoilerTemperature(desiredTemperature, currentTemperature);
         }
-   
-        else if (data == 'b') { //에어컨 강제 종료
-          digitalWrite(airConledPin, LOW);
-          airConditionerServo.attach(airConditionerServoPin);
-          airConditionerServo.write(0);
-          delay(500);
-          airConditionerServo.detach();
-          int tempflag = 0;  
-        }
-        else if (data == 'c') {
-          analogWrite(boilerLEDPin, 0);  //보일러 강제종료
-          int tempflag = 0;
+        else{
+          autoActivateAirConditioner();
         }
       }
+   
+      else if (data == 'b') { //에어컨 강제 종료
+        digitalWrite(airConledPin, LOW);
+        airConditionerServo.attach(airConditionerServoPin);
+        airConditionerServo.write(0);
+        delay(500);
+        airConditionerServo.detach();
+        int tempflag = 0;  
+      }
+      else if (data == 'c') {
+        analogWrite(boilerLEDPin, 0);  //보일러 강제종료
+        int tempflag = 0;
+      }
     }
+  
     // 스마트폰 신호가 'e' 일 경우 (원격 조정 조명 밝기 제어)
-    else if (data == 'e') {  
-    char controlValue = BTSerial.read();
-      switch (controlValue) {
+    else if (data == 'e') {
+      while(!BT.available);
+      data = BT.read();
+
+      //data의 string을 int로 변환하거나 문자열 if문으로 변경이 필요함
+      switch (data) {
         case '5':
           analogWrite(lightLEDPin, 255);
           break;
@@ -193,39 +192,70 @@ void loop() {
           break;
       }
     }
+
     // 스마트폰 신호가 'g' 일 경우 (커튼 제어)
     else if (data == 'g') {
-      while (BTSerial.available()) {
-        if (data == 'a') {
-          int curtainflag = 1;
-          Curtain();
+      while (!BTSerial.available();
+
+      if (data == 'a') {
+        int curtainflag = 1;
+        Curtain();
+      }
+      else if (data == 'b') {
+        int curtainflag = 0;
+        myStepper.step(STEPPERREV);
+        delay(3000);
+      }
+      else if (data == 'c') {
+        int curtainflag = 0;
+        myStepper.step(-STEPPERREV);
+        delay(3000);
+      }
+    }
+  }
+  //블루투스 종료
+
+  //AUTO모드 제어
+
+  //창문 제어
+  if ( WindowState == Security) {//창문상태 방범장치 상태 and 확인
+    if(irSensorValue < 200 ){ // 창문이 닫혀있고 물체가 감지되면
+      activateAlarm();  // 알람작동
+
+      if(StartTime == 0){
+        startTime = millis();
+        bool SecureFlag = true;
+      }
+
+      unsigned long currentTime = millis();
+
+      if(SecureFlag == 1){
+        if(currentTime - 5000 > startTime){
+          activateAlarm();  // 알람작동
         }
-        else if (data == 'b') {
-          int curtainflag = 0;
-          myStepper.step(STEPPERREV);
-          delay(3000);
-        }
-        else if (data == 'c') {
-          int curtainflag = 0;
-          myStepper.step(-STEPPERREV);
-          delay(3000);
+        else if(currentTime < startTime + 500 ){
+          lightAlarm();
         }
       }
     }
   }
-  // 블루투스 종료
+  else{
+    startTime = 0;
+    currentTime = 0;
+    SecureFlag = false;
+  }
 
-  // 아래부터 자동 제어
-
+  //환풍기 제어
   if (ventflag = 1 ) {
-      if (humidity > 70) { 
-        startVentMotor();                 
-      } 
-      else {
-        stopVentMotor();
-      }
+    if (humidity > 70) { 
+      startVentMotor();                 
+    } 
+    else {
+      stopVentMotor();
     }
+ }
 
+  //온도조절
   if (tempflag = 1) {
     if (desiredTemperature > currentTemperature) {
       setBoilerTemperature(desiredTemperature, currentTemperature);
@@ -234,21 +264,17 @@ void loop() {
       autoActivateAirConditioner();
     }
   }
-
+  //커튼제어
   if (curtainflag = 1) {
     Curtain();
   }
 }
-
-//main TX
-
 
 //커튼 제어 함수
 void Curtain() {
   uint16_t aValue = analogRead(Cds_Photoresistor_PIN);
   Serial.println(aValue);
   delay(100);
-
   if (aValue > 512) {
     // 정방향으로 3초 동안 회전
     myStepper.step(STEPPERREV);
@@ -261,103 +287,14 @@ void Curtain() {
   }
 }
 
-//핀 2개만 쓰기위한 라이브러리 설정
-Stepper::Stepper(int number_of_steps, int motor_pin_1, int motor_pin_2)
-{
-  this->step_number = 0;    // which step the motor is on
-  this->direction = 0;      // motor direction
-  this->last_step_time = 0; // time stamp in us of the last step taken
-  this->number_of_steps = number_of_steps; // total number of steps for this motor
 
-  // Arduino pins for the motor control connection:
-  this->motor_pin_1 = motor_pin_1;
-  this->motor_pin_2 = motor_pin_2;
-
-  // setup the pins on the microcontroller:
-  pinMode(this->motor_pin_1, OUTPUT);
-  pinMode(this->motor_pin_2, OUTPUT);
-
-  // When there are only 2 pins, set the others to 0:
-  this->motor_pin_3 = 0;
-  this->motor_pin_4 = 0;
-  this->motor_pin_5 = 0;
-
-  // pin_count is used by the stepMotor() method:
-  this->pin_count = 2;
-}
-
-void Stepper::setSpeed(long whatSpeed)
-{
-  this->step_delay = 60L * 1000L * 1000L / this->number_of_steps / whatSpeed;
-}
-
-void Stepper::step(int steps_to_move)
-{
-  int steps_left = abs(steps_to_move);  // how many steps to take
-
-  // decrement the number of steps, moving one step each time:
-  while (steps_left > 0)
-  {
-    unsigned long now = micros();
-    // move only if the appropriate delay has passed:
-    if (now - this->last_step_time >= this->step_delay)
-    {
-      // get the timeStamp of when you stepped:
-      this->last_step_time = now;
-      // increment or decrement the step number,
-      // depending on direction:
-      if (steps_to_move > 0)
-      {
-        this->step_number++;
-        if (this->step_number == this->number_of_steps) {
-          this->step_number = 0;
-        }
-      }
-      else
-      {
-        if (this->step_number == 0) {
-          this->step_number = this->number_of_steps;
-        }
-        this->step_number--;
-      }
-      // decrement the steps left:
-      steps_left--;
-      // step the motor to step number 0, 1, ..., {3 or 10}
-      stepMotor(this->step_number % 4);
-    }
-  }
-}
-
-void Stepper::stepMotor(int thisStep)
-{
-  switch (thisStep) {
-    case 0:  // 01
-      digitalWrite(motor_pin_1, LOW);
-      digitalWrite(motor_pin_2, HIGH);
-      break;
-    case 1:  // 11
-      digitalWrite(motor_pin_1, HIGH);
-      digitalWrite(motor_pin_2, HIGH);
-      break;
-    case 2:  // 10
-      digitalWrite(motor_pin_1, HIGH);
-      digitalWrite(motor_pin_2, LOW);
-      break;
-    case 3:  // 00
-      digitalWrite(motor_pin_1, LOW);
-      digitalWrite(motor_pin_2, LOW);
-      break;
-  }
-}
-
-//보알러 제어 함수
-void setBoilerTemperature(float desiredTemperature, float currentTemperature) {        
-  const float diffTemp = desiredTemperature - currentTemperature;
-  const float fadeSpeed = map(abs(diffTemp), 0, 10, 6, 1); // diffTemp에 따라 fadeSpeed 동적으로 계산
+//보일러 제어 함수
+void setBoilerTemperature(float desiredTemperature, float currentTemperature){        
+  float diffTemp = desiredTemperature - currentTemperature;
+  float fadeSpeed = map(abs(diffTemp), 0, 10, 6, 1); // diffTemp에 따라 fadeSpeed 동적으로 계산
 
   int brightness = 255; // 초기 밝기 값을 255로 설정
   analogWrite(boilerLEDPin, brightness);  // 최대 밝기로 설정
-
 
   // LED의 밝기를 31까지 부드럽게 감소
   while (brightness >= 31) {
@@ -372,21 +309,18 @@ void setBoilerTemperature(float desiredTemperature, float currentTemperature) {
     }
   }
   if (brightness < 32){
-  analogWrite(boilerLEDPin, 31);  // LED의 밝기를 63으로 유지
-  
+    analogWrite(boilerLEDPin, 31);  // LED의 밝기를 63으로 유지
   }
 }
 
 //환풍기 작동 함수
 void startVentMotor() {                    
   analogWrite(VentmotorPin, 255);  // 환풍기 DC 모터를 최대 속도로 작동
-  delay(500);
 }
 
 //환풍기 종료 함수
 void stopVentMotor() {                  
   analogWrite(VentmotorPin, 0);  // 환풍기 DC 모터를 정지
-  delay(500);
 }
 
 // 에어컨 함수
@@ -394,13 +328,12 @@ void autoActivateAirConditioner() {
  
   // 에어컨을 작동하는 코드 추가
   digitalWrite(airConledPin, HIGH);
-  delay(500);
+  delay(100);
 
   // 에어컨 작동을 표현하기 위해 서보모터를 작동시킴
-
   airConditionerServo.attach(airConditionerServoPin);
   airConditionerServo.write(90);
-  delay(500);
+  delay(100);
   airConditionerServo.detach();
 }
 
@@ -438,18 +371,27 @@ void closeWindow() {
 //창문 열기 함수
 void openWindow() {                        
   windowServo.write(90);  // 열린 위치에 해당하는 각도
-  delay(500);  
+  delay(100);  
 }
 
 //방범장치 함수
-void activateAlarm() {            
+//물체 1번 감지시 lightAlarm작동
+void lightAlarm(){
+  startTime = millis();
+  tone(buzzerPin, 1000);
+  delay(500);
+  noTone(buzzerPin);
+}
+
+//방범장치 함수 - 5초 이상 물체 감지시 10초간 0.5초동안 울렸다 멈췄다 반복
+void activateAlarm(){            
   unsigned long startTime = millis();  // 현재 시간 기록
   while (millis() - startTime < 10000) {  // 10초 동안 반복
-    tone(buzzerPin, 1000); // 피에조 부저 켜기
-    digitalWrite(ledPin, HIGH);  // 다이오드(LED) 켜기
+    unsigned long AlarmStart = millis();  // 현재 시간 기록
+    tone(buzzerPin, 1000);
     delay(500);  // 0.5초 대기
     noTone(buzzerPin);  // 피에조 부저 끄기
-    digitalWrite(ledPin, LOW);   // 다이오드(LED) 끄기
-    delay(500);  // 0.5초 대기
+    delay(500);  // 0.5초 대기 
+    AlarmStart = 0;
   }
 }
--- 개별코드/irSensor/irSensor.ino
+++ 개별코드/irSensor/irSensor.ino
@@ -1,33 +1,35 @@
-#include <Servo.h>
-#include <SoftwareSerial.h>
-
-//리모컨 제작 후 리모컨 송신코드에 따른 창문조작코드 추가 필요
-SoftwareSerial BT(2,3); //rx,tx
-
-int irSensorPin = 2;               // 적외선 센서의 핀
 int windowServoPin = 7;           // 창문 여닫이 서보모터가 연결된 핀
 int buzzerPin = 8;                 // 피에조 부저가 연결된 핀
 
+//창문상태지정 True=닫힘 False=열림
 bool WindowClose = true;;
 bool WindowAuto = true;
 bool WindowState = WindowClose;           //창문상태지정 True=닫힘 False=열림
+bool WindowState = WindowClose; 
+//창문상태지정 True=닫힘 False=열림
+bool WindowClose = true;;
+bool WindowAuto = true;
+bool WindowState = WindowClose;           //창문상태지정 True=닫힘 False=열림
+bool WindowState = WindowClose; 
+//블루투스 데이터
+char BTdata ;
+unsigned long startTime;
+unsigned long currentTime;
 
-bool SecurityOn = true;
-bool SecurityOff = false;
-bool Security = SecurityOff;
-uint8_t BTdata = 0;   
 //bt.read()의 값이 int값으로 반환되기 때문에 통신을 통해 제어하는 가장 적합한 방법이 정수형을 통한 제어라고 생각됨
+//>>앱 인벤터를 통해 데이터를 보낼경우 텍스트형식이 가장 수월하게 통신됨 그러므로 텍스트통신으로 장치제어
+
 Servo windowServo;
 
 void setup() {
-  windowServo.attach(7);  // 서보모터를 7번 핀에 연결 :
-  pinMode(irSensorPin, INPUT);
-  Serial.begin(9600);
-  BT.begin(9600);
+@@ -27,33 +36,74 @@ void setup() {
 }
 
 void loop() {
 
+//irSensorValu 업데이트
+  int irSensorValue = analogRead(2);
+//블루투스를 통해 데이터가 들어온 경우
   if(BT.available()){
     BTdata = BT.read();
     Serial.println(BTdata);
@@ -35,50 +37,101 @@ void loop() {
 
   int irSensorValue = analogRead(2);
 
+
   if(BTdata == 1){
     Security = SecurityOn;
   }
   else if(BTdata ==2){
     Security = SecurityOff;
+    
+//리모컨으로 security작동 시 동작하는 코드
+  if(BTdata == 'security'){
+
+    while(BT.available());
+
+    BTdata = BT.read();
+
+    if(BTdata == 'on'){
+      Security = SecurityOn;
+    }
+
+    else if(BTdata == 'off'){
+      Security = SecurityOff;
+    }
   }
 
+//방범장치 작동코드 - 창문이 닫혀있고, 방범장치가 켜진 상태에서 물체가 감지되면 lightAlarm,
+// 물체가 5초 이상 감지되면 10초간 알람울림
   if ( WindowState == Security) {//창문상태 방범장치 상태 and 확인
     if(irSensorValue < 200 ){ // 창문이 닫혀있고 물체가 감지되면
       activateAlarm();  // 알람작동
+
+      if(StartTime == 0){
+        startTime = millis();
+        bool SecureFlag = true;
+      }
+
+      unsigned long currentTime = millis();
+
+      if(SecureFlag == 1){
+        if(currentTime-5 > startTime){
+          activateAlarm();  // 알람작동
+        }
+        else if(currentTime < startTime + 500 ){
+          lightAlarm();
+        }
+      }
     }
+
+    else{
+      startTime = 0;
+      currentTime = 0;
+      SecureFlag = false;
+    }
+
   }
-  
+
   if(0){
    if(0){
-        openWindow();
+  if(BTdata = 'window'){
+
+    while(!BT.available());
+    BTdata = BT.read();
+
+    
+   if('open'){
+      penWindow();
     }
     else{
       closeWindow();
     }
+    
   }
 }
 
-//창문 닫기 함수
-void closeWindow() {                      
-  windowServo.write(0);  // 닫힌 위치에 해당하는 각도
-  delay(500);  // 서보모터가 움직이기를 기다림
-  WindowState = true; // 창문상태 닫힘
-}
-
-//창문 열기 함수
-void openWindow() {                        
-  windowServo.write(90);  // 열린 위치에 해당하는 각도
-  delay(500);  // 서보모터가 움직이기를 기다림
+void openWindow() {
   WindowState = false; //창문상태 열림]
 }
 
 //방범장치 함수
+//물체 1번 감지시 lightAlarm작동
+void lightAlarm(){
+  startTime = millis();
+  tone(buzzerPin, 1000);
+  delay(500);
+  noTone(buzzerPin);
+}
+
+//방범장치 함수 - 5초 이상 물체 감지시 10초간 0.5초동안 울렸다 멈췄다 반복
 void activateAlarm() {            
   unsigned long startTime = millis();  // 현재 시간 기록
   while (millis() - startTime < 10000) {  // 10초 동안 반복
+  unsigned long AlarmStart = millis();  // 현재 시간 기록
+  while (millis() - AlarmStart < 10000) {  // 10초 동안 반복
     tone(buzzerPin, 1000);
     delay(500);  // 0.5초 대기
     noTone(buzzerPin);  // 피에조 부저 끄기
     delay(500);  // 0.5초 대기
   }
+  AlarmStart = 0;
 }
