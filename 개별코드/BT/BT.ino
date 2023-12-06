728b71a6643383b6911ba7a692cf0e90bb70c253
YoungkiKim <gkdlgkdl1231@gmail.com>
2023-12-06 16:49:38 +0900

    BTadd

--- 개별코드/BT/BT.ino
+++ 개별코드/BT/BT.ino
@@ -1,25 +1,25 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial BTserial(2,3); // RX, TX
-uint8_t data = 0x00;
+//uint8_t data = 0x00;
+
+
+char data ; 
 
 void setup() {
-  Serial.begin(38400);
-  BTserial.begin(38400);
+
+  Serial.begin(9600);
+  BTserial.begin(9600);
 }
 
 void loop() {
 
   if(BTserial.available()){
-    Serial.write(BTserial.read());
-    
+    data = BTserial.read() ;
+    Serial.write(data);
   }
 
   if( Serial.available() ){
     BTserial.write( Serial.read() );
-   
   }
 }
-
-
-//main TX
