e60ae53d9f6fd91b8aa33d93b4381be6aed19ddd
YoungkiKim <gkdlgkdl1231@gmail.com>
2023-12-17 14:30:09 +0900

    아두이노 통신 수정 소프트웨어 인터럽트에서 신호처리까지 하던 것 loop문에서 데이터처리로 수정 기본데이터 y y가 아닐 시 데이터의 변경을 감지하고 데이터 탐지. 창문 Servo모터 핀 9번으로 수정 필요

--- 개별코드/assemble/1.개별 아두이노 코드/FirstArduino/FirstArduino.ino
+++ 개별코드/assemble/1.개별 아두이노 코드/FirstArduino/FirstArduino.ino
@@ -24,6 +24,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C 주소 0x27에 연결된 16x2 LCD
 float desiredTemperature;  // 추가: desiredTemperature 변수 선언
 float humidity;  // 추가: humidity 변수 선언
 bool Tempflag;
+char data;
 
 //창문변수들
 //창문상태
@@ -41,7 +42,7 @@ unsigned long alarmcurrent;
 unsigned long currentTime;
 unsigned long exTF;
 //핀설정
-int windowServoPin = 7;             // 창문 여닫이 서보모터가 연결된 핀
+int windowServoPin = 9;             // 창문 여닫이 서보모터가 연결된 핀
 int buzzerPin = 8;                 // 피에조 부저가 연결된 핀
 int irSensorValue; // irSensorValue 변수 선언 
 //방범상태
@@ -64,13 +65,16 @@ void setup(){
   attachInterrupt(digitalPinToInterrupt(2),SoftwareISR,FALLING);
   attachInterrupt(digitalPinToInterrupt(0),SoftwareISRRx,FALLING);
 }
-
+w
 void loop(){
-  float humidity = dht.readHumidity();    
-  float currentTemperature = dht.readTemperature();     
-  
-  //두번째 아두이노로 온습도 희망온도 전송 1초마다
-  if(exTF> millis()-1000){
+  Serial.println("loop");
+  humidity = dht.readHumidity();    
+  currentTemperature = dht.readTemperature();
+  //Serial.print(currentTemperature);
+  irSensorValue = analogRead(2);
+
+  //두번째 아두이노로 온습도 희망온도 전송 3초마다
+  if(exTF> millis()-3000){
     Serial.print(currentTemperature);
     Serial.print(",");
     Serial.print(humidity);
@@ -83,121 +87,129 @@ void loop(){
   //환풍기제어
   if (ventflag == 1 ) {
     if (humidity > 70) {
-      analogWrite(VentmotorPin, 255);  // 환풍기 DC 모터를 최대 속도로 작동
+      digitalWrite(VentmotorPin, 255);  // 환풍기 DC 모터를 최대 속도로 작동
       ventState= true;
     }
     else {
-        analogWrite(VentmotorPin, 0);  // 환풍기 DC 모터를 정지
-        ventState= false;
+      digitalWrite(VentmotorPin, 0);  // 환풍기 DC 모터를 정지
+      ventState= false;
     }
   }
 
   //방범
   //창문 제어
-  if ( WindowState == Security) {//창문상태 방범장치 상태 and 확인
-    if(irSensorValue < 200 ){ // 창문이 닫혀있고 물체가 감지되면
-      if(startTime == 0){
-        startTime = millis();
-        SecureFlag = true;
-      }
+  if ( WindowState == WindowClose) {//창문상태 방범장치 상태 and 확인
+    if(Security == true){
+      if(irSensorValue < 200 ){ // 창문이 닫혀있고 물체가 감지되면
+        if(startTime == 0){
+          startTime = millis();
+          SecureFlag = true;
+        }
 
-      unsigned long currentTime = millis();
-      if(SecureFlag == true){
-        if(currentTime - 5000 > startTime){
-          if(AlarmFlag == 0){
-            actstartTime = millis();
-            AlarmFlag = true;
-            alarmStart = millis();  // 현재 시간 기록
-          } 
-          if(AlarmFlag == true){
-            alarmcurrent = millis();
-            activateAlarm();  // 알람작동
+        unsigned long currentTime = millis();
+        if(SecureFlag == true){
+          if(currentTime - 5000 > startTime){
+            if(AlarmFlag == 0){
+              actstartTime = millis();
+              AlarmFlag = true;
+              alarmStart = millis();  // 현재 시간 기록
+            } 
+
+            if(AlarmFlag == true){
+              alarmcurrent = millis();
+              activateAlarm();  // 알람작동
+            }
+          }
+          else if(currentTime < startTime + 500 ){
+            lightAlarm();
           }
         }
-        else if(currentTime < startTime + 500 ){
-          lightAlarm();
-        }
-      }
+      } 
       
-    } 
-    
-    else{
-      startTime = 0;
-      currentTime = 0;
-      SecureFlag = false;
-      AlarmFlag = false;
-      noTone(buzzerPin);
+      else{
+        startTime = 0;
+        currentTime = 0;
+        SecureFlag = false;
+        AlarmFlag = false;
+        noTone(buzzerPin);
+      }
     }
   }
-}
+  Serial.print("check data: " );
+  Serial.println(data);
 
-void SoftwareISRRx(){
-  char data;
-  while(BTserial.available()){
-    data = BTserial.read();
-  }
-  updateTemp = data.toFloat();
-}
-
-void SoftwareISR(){
-  while(BTserial.available()){
-
-    char data = BTserial.read();
-
-    if(data == 'a'){Security = true;}
-    else if(data == 'b'){Security = false;}
+  if( !(data == 'y') ){
+    if(data == 'a'){
+    Security = !Security;
+    Serial.println(Security);
+    noTone(buzzerPin);
+    data = 'y';
+    }
+    else if(data == 'b'){
+      Security = false;
+      data = 'y';
+    }
     //환풍기 AUTO ON OFF
     else if(data == 'c'){ventflag = 1;}
     else if(data == 'd'){
-      analogWrite(VentmotorPin, 255);
+      Serial.println("check d");
+      digitalWrite(VentmotorPin, HIGH);
       ventflag = 0;
+      data = 'y';
     }
     else if(data == 'e'){
       analogWrite(VentmotorPin,0);
       ventflag = 0;
+      data = 'y';
     }
     else if(data == 'f'){
       openWindow();
+      data = 'y';
     }
     else if(data == 'g'){
       closeWindow();
+      data = 'y';
     }
     else if(data == 'h'){
-      int tempflag = 1;
-      Serial.print(h);
+      Tempflag = 1;
+      Serial.print("h");
+      data = 'y';
     }
-  //   else if(data == 'i'){
-  //     digitalWrite(airConledPin, LOW);
-  //     airConditionerServo.attach(airConditionerServoPin);
-  //     airConditionerServo.write(0);
-  //     delay(500);
-  //     airConditionerServo.detach();
-  //     int tempflag = 0;
-  //   }
-  //   else if(data == 'j'){
-  //     analogWrite(boilerLEDPin, 0);  //보일러 강제종료
-  //     int tempflag = 0;
-  //   }
-  //   else if(data == 'k'){}//에어컨 터보
-  //   else if(data == 'l'){}//보일러 터보
-  //   else if(data == 'm'){}
-  //   else if(data == 'n'){}
-  //   else if(data == 'o'){}
-
-  //   //조명
-  //   else if(data == '0'){analogWrite(lightLEDPin,LOW);}//off
-  //   else if(data == '1'){analogWrite(lightLEDPin,32);}//off
-  //   else if(data == '2'){analogWrite(lightLEDPin,64);}//off
-  //   else if(data == '3'){analogWrite(lightLEDPin,128);}//off
-  //   else if(data == '4'){analogWrite(lightLEDPin,196);}//off
-  //   else if(data == '5'){analogWrite(lightLEDPin,255);}//off
-  //}
     else if(Serial.find(".")){
-      desiredTemperature = toFloat();
+      desiredTemperature = data;
+      Serial.println(desiredTemperature);
+      Tempflag = 1;
+      Serial.print("z");
+      data = 'y';
+    }
+    else if(data == 'x'){
+      BTserial.print(currentTemperature);
+      BTserial.println("'C");
+      BTserial.print(humidity);
+      BTserial.println("%");
+      Serial.print("x");
+      data = 'y';
     }
     else{
       Serial.print(data);
+      data = 'y';
     }
+  }
+  delay(1000);
+}
+
+void SoftwareISRRx(){
+  data;
+  while(BTserial.available()){
+    data = BTserial.read();
+  }
+  updateTemp = data;
+}
+
+void SoftwareISR(){
+  while(BTserial.available()){
+    data = BTserial.read();
+    break;
   }
 }
 
@@ -205,15 +217,13 @@ void SoftwareISR(){
 void displayTemperatureAndHumidity(float currentTemperature,float humidity ){
   lcd.setCursor(0, 0);//0행 실내온도
   lcd.print("Temp: ");
-  if(Tempflag == True){
+  if(Tempflag == true){
     lcd.print(updateTemp);
   }
   else{
-    lcd.print(currentTemp);
+    lcd.print(currentTemperature);
   }
-  lcd.print(currentTemperature);
   lcd.print(" C");
-  Serial.println(" Check lcd");
   lcd.setCursor(0, 1);   //1행 실내습도
   lcd.print("Humidity: ");
   lcd.print(humidity);
@@ -221,7 +231,20 @@ void displayTemperatureAndHumidity(float currentTemperature,float humidity ){
 }
 
 void activateAlarm(){
-  if(alarmcurrent - actstartTime < 10000){  // 현재 시간 기록 8000 ){
+  if(alarmcurrent - actstartTime < 2000){  // 현재 시간 기록 8000 ){
+    Serial.println("activeAlram");
+    tone(buzzerPin, 500); 
+  }
+  else if(alarmcurrent - actstartTime < 4000){
+    noTone(buzzerPin); 
+  }
+  else if(alarmcurrent - actstartTime < 6000){
+    tone(buzzerPin, 700); 
+  }
+  else if(alarmcurrent - actstartTime < 8000){
+    noTone(buzzerPin); 
+  }
+  else if(alarmcurrent - actstartTime < 15000){
     tone(buzzerPin, 1000); 
   }
   else{
@@ -236,7 +259,8 @@ void lightAlarm(){
   startTimelight = millis();
   Serial.println("liht arlded: ");
   while (millis() - startTimelight < 1000){
-    tone(buzzerPin, 1000);
+    Serial.println("lightAlarm");
+    tone(buzzerPin, 200);
     delay(500);
     noTone(buzzerPin);
   }
@@ -244,15 +268,16 @@ void lightAlarm(){
 
 //창문 열기 함수
 void openWindow(){
-  windowServo.attach(7);
+  windowServo.attach(9);
+  windowServo.write(80);
   delay(1000); 
-  detach();
-  windowServo.WindowState= false;
+  windowServo.detach();
+  WindowState= false;
 }
 
 //창문 닫기 함수
 void closeWindow(){
-  windowServo.attach(7);
+  windowServo.attach(9);
   windowServo.write(0);  // 닫힌 위치에 해당하는 각도
   delay(1000);
   windowServo.detach();
